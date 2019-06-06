d1abd4dc56fbbddd36750d57989bf058f7f3c4a2
Fredy Fuentes <fredy.fuentesb@gmail.com>
2019-06-05 22:40:42 -0500

    Aplicacion de ejemplo de crud con firebase terminada

--- src/app/pages/heroe/heroe.component.ts
+++ src/app/pages/heroe/heroe.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { HeroeModel } from '../../models/heroe.model';
 import { NgForm } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+
+import { HeroeModel } from '../../models/heroe.model';
+import { HeroesService } from '../../services/heroes.service';
+
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-heroe',
@@ -11,18 +17,48 @@ export class HeroeComponent implements OnInit {
 
   heroe = new HeroeModel();
 
-  constructor() { }
+  constructor(private heroeService: HeroesService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id !== 'nuevo') {
+      this.heroeService.obtenerHeroe(id).subscribe((resp: HeroeModel) => {
+        this.heroe = resp;
+        this.heroe.id = id;
+      });
+    }
   }
 
   guardar(form: NgForm) {
-    if(form.invalid){
+    if (form.invalid) {
       console.log('formulario no valido');
       return;
     }
-    console.log(form);
-    console.log(this.heroe);
+
+    Swal.fire({
+      title: 'Espere',
+      text: 'Guardando informacion',
+      type: 'info',
+      allowOutsideClick: false
+    });
+
+    Swal.showLoading();
+
+    let peticion: Observable<any>;
+
+    if (this.heroe.id) {
+      peticion = this.heroeService.actualizarHeroe(this.heroe);
+    } else {
+      peticion = this.heroeService.crearHeroe(this.heroe);
+    }
+
+    peticion.subscribe(resp => {
+      Swal.fire({
+        title: this.heroe.nombre,
+        text: 'Se actualizó correctamente',
+        type: 'success'
+      });
+    });
   }
 
 }
--- src/app/pages/heroes/heroes.component.ts
+++ src/app/pages/heroes/heroes.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
+import { HeroeModel } from '../../models/heroe.model';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-heroes',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  heroes: HeroeModel [] = [];
+  cargando = false;
+
+  constructor(private heroService: HeroesService) { }
 
   ngOnInit() {
+    this.cargando = true;
+    this.heroService.obtenerHeroes().subscribe(resp => {
+      this.heroes = resp;
+      this.cargando = false;
+    });
+  }
+
+  eliminarHeroe(heroe: HeroeModel, idx: number) {
+    Swal.fire({
+      title: `¿Está seguro de borrar a ${ heroe.nombre }?`,
+      text: `¿Está seguro de borrar a ${ heroe.nombre }?`,
+      type: 'question',
+      showConfirmButton: true,
+      showCancelButton: true
+    }).then(resp => {
+      if (resp.value) {
+        this.heroes.splice(idx, 1);
+        this.heroService.eliminarHeroe(heroe.id).subscribe();
+      }
+    });
   }
 
 }
